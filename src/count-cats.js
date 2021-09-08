af5cf190261fb8bb95184bf31feb619666e5b58a
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2021-09-08 19:12:10 +0300

    basic-js

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  matrix = matrix.flat()
+  var count = 0
+  for (var i in matrix) {
+    if (matrix[i] === "^^") {
+      count++
+    }
+  }
+  return count
 }
