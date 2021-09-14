a1958af4c91f8a63e966117b6dee68face8ecbac
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2021-09-14 17:54:49 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,26 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  // if (!arr.isArray) {
+  //   throw '\'arr\' parameter must be an instance of the Array!'
+  // }
+  for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i] === '--double-next') {
+      arr[i] = arr[i + 1]
+    }
+    if (arr[i] === '--double-prev') {
+      arr[i] = arr[i - 1]
+    }
+    if (arr[i] === '--discard-next') {
+      delete arr[i]
+      delete arr[i + 1]
+    }
+    if (arr[i] === '--discard-prev') {
+      delete arr[i]
+      delete arr[i - 1]
+    }
+  }
+  arr = arr.filter(elem => elem != null)
+  return arr
 }
