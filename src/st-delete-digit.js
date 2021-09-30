cd06e24f2fed88b438f562dca1812695b705b953
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2021-09-30 18:44:50 +0300

    Update st-delete-digit.js

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+   n = n.toString().split('')
+   var arr = [], n_copy = [], res_arr = []
+   for (var i = 0; i < n.length; i++) {
+      n_copy.push(n[i])
+      n[i] = ','
+      arr = n.filter(elem => elem !== ',')
+      n[i] = n_copy[i]
+      res_arr.push(+arr.join(''))
+   }
+   var max_elem = Math.max.apply(null, res_arr)
+   return max_elem
 }
