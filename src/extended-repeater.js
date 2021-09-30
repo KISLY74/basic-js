acd93196e8475509fbf4a4c92f3db47a290fbed0
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2021-10-01 01:14:42 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  var arr = []
+  for (var i in options) {
+    arr.push(options[i])
+  }
+  var arr_sub = [], arr_full = [], arr_big = []
+  for (var i = 0; i < arr[3]; i++)
+    arr_sub.push(arr[2])
+  arr_sub = arr_sub.join(`${arr[4]}`)
+  arr_full.push((str + arr_sub))
+  for (var i = 0; i < arr[0]; i++)
+    arr_big.push(arr_full)
+  arr_big = arr_big.join(`${arr[1]}`)
+  return arr_big
 }
